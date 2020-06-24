ac5e93b87f1a199f2e5c19827c24d0f43a969a60
Fábio Mendes <ms.fabiomendes@gmail.com>
2020-06-23 22:00:01 -0300

    Fix UserController export

--- server/src/controller/UserController.ts
+++ server/src/controller/UserController.ts
@@ -2,7 +2,7 @@ import { getRepository } from "typeorm";
 import { NextFunction, Request, Response } from "express";
 import { User } from "../entity/User";
 
-export class UserController {
+class UserController {
   private userRepository = getRepository(User);
 
   async all(request: Request, response: Response, next: NextFunction) {
@@ -22,3 +22,5 @@ export class UserController {
     await this.userRepository.remove(userToRemove);
   }
 }
+
+export default new UserController();
