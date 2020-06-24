91ab75d3ab51b52df488eef5b6483ec52cd6afda
Fábio Mendes <ms.fabiomendes@gmail.com>
2020-06-23 22:02:54 -0300

    Refactor Express routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,7 @@ import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
-import { Routes } from "./routes";
-import { User } from "./entity/User";
+import routes from "./routes";
 
 createConnection()
   .then(async (connection) => {
@@ -13,27 +12,7 @@ createConnection()
     app.use(bodyParser.json());
 
     // register express routes from defined application routes
-    Routes.forEach((route) => {
-      (app as any)[route.method](
-        route.route,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
-          );
-          if (result instanceof Promise) {
-            result.then((result) =>
-              result !== null && result !== undefined
-                ? res.send(result)
-                : undefined
-            );
-          } else if (result !== null && result !== undefined) {
-            res.json(result);
-          }
-        }
-      );
-    });
+    app.use(routes);
 
     // start express server
     app.listen(3333);
